4c9821789ba4b4fd8f61d458cb067d7e3ac0d8dc
qian <920852031@qq.com>
2022-03-20 20:01:44 +0800

    谷粒商城基本框架

---
+++ gulimall-member/src/test/java/com/qian/gulimall/gulimallmember/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qian.gulimall.gulimallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
