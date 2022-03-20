48c5a20da8142749d20c9ba9917bd387061f3c4b
qian <920852031@qq.com>
2022-03-20 20:01:44 +0800

    谷粒商城各项微服务实现基本sql

--- gulimall-product/src/main/java/com/qian/gulimallproduct/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/qian/gulimallproduct/GulimallProductApplication.java
@@ -1,8 +1,10 @@
 package com.qian.gulimallproduct;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.qian.gulimallproduct.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/qian/gulimallproduct/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/qian/gulimallproduct/GulimallProductApplicationTests.java
@@ -1,13 +1,21 @@
 package com.qian.gulimallproduct;
 
+import com.qian.gulimallproduct.entity.BrandEntity;
+import com.qian.gulimallproduct.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
-
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("huawei");
+        brandService.save(brandEntity);
+        System.out.println("插入成功");
     }
 
 }
